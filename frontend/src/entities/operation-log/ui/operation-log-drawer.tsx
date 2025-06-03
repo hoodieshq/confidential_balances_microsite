02c7773c75e9ee4f7eb08bf402f0de0dc9ce4345
Sergey Zwezdin <sergey@zwezdin.com>
2025-06-03 16:15:37 +0300

    feat: store for operation log

--- frontend/src/entities/operation-log/ui/operation-log-drawer.tsx
+++ frontend/src/entities/operation-log/ui/operation-log-drawer.tsx
@@ -1,54 +1,28 @@
 'use client'
 
-import { FC, useEffect, useState } from 'react'
-import { useAtom } from 'jotai'
+import { FC, use, useEffect, useMemo, useState } from 'react'
+import { useAtom, useAtomValue } from 'jotai'
 import { OperationLogDrawer as OperationLogDrawerBase } from '@/shared/ui/operation-log-drawer'
+import { MAX_LOG_ITEMS, operationLogAtom, OperationLogItem } from '../model/operation-log-items'
 import { operationLogOpenAtom } from '../model/operation-log-open'
-
-const items: {
-  title: string
-  content: string
-  variant: 'success' | 'error' | 'muted'
-}[] = [
-  {
-    title: 'Welcome to Solana Confidential Balances operation log!',
-    content: `Here, you'll find a transparent record of all your recent activity.\nEvery operation you approve will be logged here for your reference.`,
-    variant: 'muted',
-  },
-  {
-    title: 'Deposit Operation - COMPLETE',
-    content: `Txn1 [SUCCESS]\n  ConfidentialTransferInstruction::Deposit\n    Note: Deposited 12 tokens`,
-    variant: 'success',
-  },
-  {
-    title: 'Deposit Operation - COMPLETE',
-    content: `Txn1 [SUCCESS]\n  ConfidentialTransferInstruction::Deposit\n    Note: Deposited 12 tokens`,
-    variant: 'success',
-  },
-  {
-    title: 'Deposit Operation - COMPLETE',
-    content: `Txn1 [SUCCESS]\n  ConfidentialTransferInstruction::Deposit\n    Note: Deposited 12 tokens`,
-    variant: 'success',
-  },
-  {
-    title: 'Deposit Operation - COMPLETE',
-    content: `Txn1 [SUCCESS]\n  ConfidentialTransferInstruction::Deposit\n    Note: Deposited 12 tokens`,
-    variant: 'success',
-  },
-  {
-    title: 'Deposit Operation - COMPLETE',
-    content: `Txn1 [SUCCESS]\n  ConfidentialTransferInstruction::Deposit\n    Note: Deposited 12 tokens`,
-    variant: 'success',
-  },
-  {
-    title: 'Deposit Operation - COMPLETE',
-    content: `Txn1 [SUCCESS]\n  ConfidentialTransferInstruction::Deposit\n    Note: Deposited 12 tokens`,
-    variant: 'error',
-  },
-]
 
 export const OperationLogDrawer: FC = () => {
   const [operationLogOpen, setOperationLogOpen] = useAtom(operationLogOpenAtom)
+  const log = useAtomValue(operationLogAtom)
+  const items = useMemo<OperationLogItem[]>(
+    () =>
+      log.length < MAX_LOG_ITEMS
+        ? [
+            {
+              title: 'Welcome to Solana Confidential Balances operation log!',
+              content: `Here, you'll find a transparent record of all your recent activity.\nEvery operation you approve will be logged here for your reference.`,
+              variant: 'muted',
+            },
+            ...log,
+          ]
+        : log,
+    [log]
+  )
 
   return (
     <OperationLogDrawerBase
