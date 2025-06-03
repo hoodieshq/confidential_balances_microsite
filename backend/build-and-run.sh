c68d0bc66fbec3ef351804822ca952ec3c9726a8
Sergo <rogaldh@radsh.red>
2025-06-03 12:09:29 +0100

    remove -i

--- backend/build-and-run.sh
+++ backend/build-and-run.sh
@@ -32,7 +32,7 @@ docker build $PLATFORM_FLAG -t $IMAGE_TAG . || {
 }
 
 echo "Running container locally on port 3003..."
-docker run $PLATFORM_FLAG -p 3003:3003 -e PORT=3003 $IMAGE_TAG -i
+docker run $PLATFORM_FLAG -p 3003:3003 -e PORT=3003 $IMAGE_TAG
 
 # If this is a production build, output the command to push to Cloud Run
 if [[ "$1" == "--prod" || "$1" == "-p" ]]; then
