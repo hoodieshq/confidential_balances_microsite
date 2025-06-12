ccddc79081a4e1925892f0dca5f900690833bcee
Sergey Zwezdin <sergey@zwezdin.com>
2025-06-12 17:31:06 +0300

    fix: wallet adapters added

--- frontend/src/app/providers/solana-provider.tsx
+++ frontend/src/app/providers/solana-provider.tsx
@@ -8,7 +8,82 @@ import dynamic from 'next/dynamic'
 import { WalletError } from '@solana/wallet-adapter-base'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import {
+  AlphaWalletAdapter,
+  AvanaWalletAdapter,
+  BitgetWalletAdapter,
+  BitpieWalletAdapter,
+  CloverWalletAdapter,
+  Coin98WalletAdapter,
+  CoinbaseWalletAdapter,
+  CoinhubWalletAdapter,
+  FractalWalletAdapter,
+  HuobiWalletAdapter,
+  HyperPayWalletAdapter,
+  KeystoneWalletAdapter,
+  KrystalWalletAdapter,
+  LedgerWalletAdapter,
+  MathWalletAdapter,
+  NekoWalletAdapter,
+  NightlyWalletAdapter,
+  NufiWalletAdapter,
+  OntoWalletAdapter,
+  ParticleAdapter,
+  PhantomWalletAdapter,
+  SafePalWalletAdapter,
+  SaifuWalletAdapter,
+  SalmonWalletAdapter,
+  SkyWalletAdapter,
+  SolflareWalletAdapter,
+  SolongWalletAdapter,
+  SpotWalletAdapter,
+  TokenaryWalletAdapter,
+  TokenPocketWalletAdapter,
+  TorusWalletAdapter,
+  TrezorWalletAdapter,
+  TrustWalletAdapter,
+  UnsafeBurnerWalletAdapter,
+  XDEFIWalletAdapter,
+} from '@solana/wallet-adapter-wallets'
 import { useCluster } from '@/shared/solana'
+
+const wallets = [
+  new AlphaWalletAdapter(),
+  new AvanaWalletAdapter(),
+  new BitgetWalletAdapter(),
+  new BitpieWalletAdapter(),
+  new CloverWalletAdapter(),
+  new Coin98WalletAdapter(),
+  new CoinbaseWalletAdapter(),
+  new CoinhubWalletAdapter(),
+  new FractalWalletAdapter(),
+  new HuobiWalletAdapter(),
+  new HyperPayWalletAdapter(),
+  new KeystoneWalletAdapter(),
+  new KrystalWalletAdapter(),
+  new LedgerWalletAdapter(),
+  new MathWalletAdapter(),
+  new NekoWalletAdapter(),
+  new NightlyWalletAdapter(),
+  new NufiWalletAdapter(),
+  new OntoWalletAdapter(),
+  new ParticleAdapter(),
+  new PhantomWalletAdapter(),
+  new SafePalWalletAdapter(),
+  new SaifuWalletAdapter(),
+  new SalmonWalletAdapter(),
+  new SkyWalletAdapter(),
+  new SolflareWalletAdapter(),
+  new SolongWalletAdapter(),
+  new SpotWalletAdapter(),
+  new TokenaryWalletAdapter(),
+  new TokenPocketWalletAdapter(),
+  new TorusWalletAdapter(),
+  new TrezorWalletAdapter(),
+  new TrustWalletAdapter(),
+  new UnsafeBurnerWalletAdapter(),
+  new XDEFIWalletAdapter(),
+]
 
 export const WalletButton = dynamic(
   async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
@@ -26,7 +101,7 @@ export const SolanaProvider: FC<PropsWithChildren> = ({ children }) => {
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={[]} onError={onError} autoConnect>
+      <WalletProvider wallets={wallets} onError={onError} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
