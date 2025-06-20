8e941dfa004c5900d39bcd9a48cc97a266638c1a
Sergo <rogaldh@radsh.red>
2025-06-20 13:07:45 +0100

    chore: remove deprecated and somme barrel files

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -2,7 +2,7 @@
 
 import { FC, type PropsWithChildren } from 'react'
 import { Toaster } from '@solana-foundation/ms-tools-ui/components/sonner'
-import { WalletConnection } from '@/entities/wallet/connection'
+import { WalletConnection } from '@/entities/wallet/connection/ui/wallet-connection'
 import { ClusterProvider } from './providers/cluster-provider'
 import { SolanaProvider } from './providers/solana-provider'
 import { JotaiProvider, ReactQueryProvider } from './providers/state-providers'
